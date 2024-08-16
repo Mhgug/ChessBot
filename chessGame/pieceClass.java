df69fd5d985f542385ee91b7875315dbae4ef4d1
Mhgug <eryigitjohn@gmail.com>
2024-08-16 15:14:55 -0400

    start

---
+++ chessGame/pieceClass.java
@@ -0,0 +1,13 @@
+/*
+ * pieceChess.java
+ * Classes for chess peices
+ * 8/12/2024
+ */
+
+package chessGame;
+
+public class pieceClass {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
