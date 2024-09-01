b048de2d963832326072f44a6410d7b1d95c9811
Mhgug <eryigitjohn@gmail.com>
2024-08-31 20:51:45 -0400

    Worked on panel

--- Panel.java
+++ Panel.java
@@ -16,33 +16,33 @@ class Panel extends JPanel {
 
     public void boardRepresent(Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
-
+    
         int x = 0;
         int y = 0;
-
+    
         g2d.setPaint(new Color(0, 0, 0));
         for(int i = 0; i < 8; i++) {
-
+    
             for(int j = 0; j < 8; j++) {
                 g2d.fillRect(x, y, 50, 50);
                 x = 100 * j;
             }
-    
+        
             y = 100 * i;
-
+    
         }
-
+    
         x = 50;
         y = 0;
-
+    
         for(int i = 0; i < 8; i++) {
+    
+            y = 100 * i + 50;
+                
             for(int j = 0; j < 8; j++) {
                 g2d.fillRect(x, y, 50, 50);
                 x = 100 * j + 50;
             }
-
-            y = 100 * i + 50;
-
         }
     }
 }
